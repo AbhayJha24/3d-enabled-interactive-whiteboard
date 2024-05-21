6b46f9ab6355ccb1482a632240f5ac211b9776c8
Abhay <abhay.jha243@gmail.com>
2024-05-21 12:10:37 +0530

    Fixed broken URL

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
     <React.StrictMode>
       <HashRouter>
       <Routes>
-      <Route path="/" exact={true} element={<><iframe src="models" className="featuresModels" frameBorder="0" title="3D Models" allowFullScreen></iframe><Suspense fallback={<div className="modelLoadingSpinner"></div>}></Suspense></>} />
+      <Route path="/" exact={true} element={<><iframe src="https://3d-enabled-interactive-whiteboard.vercel.app/#/models" className="featuresModels" frameBorder="0" title="3D Models" allowFullScreen></iframe><Suspense fallback={<div className="modelLoadingSpinner"></div>}></Suspense></>} />
       <Route path="models" exact={true} element={<><Suspense fallback={<div className="modelLoadingSpinner"></div>}><Models /></Suspense></>} />
       <Route path="*" exact={true} element={<><h1 style={{color: "#000000", margin: "auto",textAlign: "center"}}>404<br />Not Found</h1></>} />
       </Routes>
